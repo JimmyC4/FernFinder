bb64960fb4adb0fc42ea388a55d637909821c650
sarahlevinscurtin <93862200+sarahlevinscurtin@users.noreply.github.com>
2022-11-16 19:26:38 +0800

    fixes

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from "next/Link";
+import Link from "next/link";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {auth} from '../utils/firebase';
 // import { auth } from 'firebase/app';
