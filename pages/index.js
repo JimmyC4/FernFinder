d8aba796d0b3986141ff7d57533eb7da492e77a2
JimmyC4 <jameskang99@outlook.com>
2022-11-23 19:09:59 +0800

    updating

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner}
 from '../components';
+import Iframe from 'react-iframe';
 
 const Home = ({ products, bannerData }) => {
   return (
@@ -18,6 +19,35 @@ const Home = ({ products, bannerData }) => {
           {product._id} product={product} />)}
         </div>
       </section>
+
+      <div className='center_container'>
+        <h1 className='heading'>Here's a selection of videos to help <br/>nurture your knowledge.</h1>
+      </div>
+      
+      <div className='video_container'>
+        <div className='video'>
+          <Iframe
+              url="https://www.youtube.com/embed/pTCsabfYWJA?autoplay=0&mute=0&rel=0&controls=0&showinfo=0&modestbranding=1&VQ=HD1080&loop=1}" loop="1" frameBorder="1" allowFullScreen position="relative" playsInline
+              width="100%" height="100%" />
+        </div>
+        <div className='video'>
+          <Iframe
+              url="https://www.youtube.com/embed/LZhnCxG5c6s?autoplay=0&mute=0&rel=0&controls=0&showinfo=0&modestbranding=1&VQ=HD1080&loop=1}" loop="1" frameBorder="0" allowFullScreen position="relative" playsInline
+              width="100%" height="100%" />
+        </div>
+        <div className='video'>
+          <Iframe
+              url="https://www.youtube.com/embed/ugKWkJyGFQg?autoplay=0&mute=0&rel=0&controls=0&showinfo=0&modestbranding=1&VQ=HD1080&loop=1}" loop="1" frameBorder="0" allowFullScreen position="relative" playsInline
+              width="100%" height="100%" />
+        </div>
+        <div className='video'>
+          <Iframe
+              url="https://www.youtube.com/embed/Xb5OkAVT09M?autoplay=0&mute=0&rel=0&controls=0&showinfo=0&modestbranding=1&VQ=HD1080&loop=1%7D}" loop="1" frameBorder="0" allowFullScreen position="relative" playsInline
+              width="100%" height="100%" />
+        </div>
+        
+      </div>
+      
 
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
